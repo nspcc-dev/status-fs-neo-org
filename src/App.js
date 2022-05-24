bdcd3e90822394bf52ea59852af092ff34f4ab28
Mikhail Petrov <mike@nspcc.ru>
2022-05-24 16:41:39 +0300

    [#1] Get data by fetch request
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'bulma/css/bulma.min.css';
 import {
 	Navbar,
@@ -30,7 +30,7 @@ import {
   Circle,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import data from "./output.json";
+
 import './App.css';
 
 ChartJS.register(
@@ -45,6 +45,7 @@ ChartJS.register(
 export const App = () => {
 	const [activeNet, setActiveNet] = useState('mainnet');
 	const [menuActive, setMenuActive] = useState(false);
+	const [data, setData]=useState();
 
 	const [chartData, setChartData] = useState({
 		labels: [],
@@ -53,6 +54,17 @@ export const App = () => {
 			data: [],
 		}],
 	});
+
+  useEffect(()=>{
+    fetch('output.json', {
+			'headers' : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+       }
+    }).then((response) => response.json()).then((myJson) => {
+			setData(myJson);
+		});
+  },[]);
 
   const onScroll = (event, block) => {
     const scrollDiv = document.getElementById(block).offsetTop + 30;
@@ -98,221 +110,225 @@ export const App = () => {
 					</Navbar.Container>
 					<Navbar.Container align="right">
 						<Navbar.Item renderAs="div" style={{ fontSize: 14 }}>
-							{`${new Date(data.time * 1000).toLocaleTimeString()} ${new Date(data.time * 1000).toLocaleDateString()}`}
+							{data ? `${new Date(data.time * 1000).toLocaleTimeString()} ${new Date(data.time * 1000).toLocaleDateString()}` : ''}
 						</Navbar.Item>
 					</Navbar.Container>
 				</Navbar.Menu>
 			</Navbar>
-			<Container>
-				<Section>
-					<Tile
-						kind="child"
-						renderAs={Notification}
-						color={getColorStatus(data.status)}
-					>
-						<Heading>{`Status: ${data.status}`}</Heading>
-					</Tile>
-					<Box style={{ marginTop: '1.5rem' }} id="main">
-						<Tile kind="ancestor">
-							<Tile kind="parent">
-								<Tile
-									kind="child"
-									renderAs={Notification}
-									color="grey"
-								>
-									<Heading subtitle>
-										{`Network epoch: `}
-										<span>{data.network_epoch}</span>
-									</Heading>
-								</Tile>
-							</Tile>
+			{data && (
+				<>
+					<Container>
+					<Section>
+						<Tile
+							kind="child"
+							renderAs={Notification}
+							color={getColorStatus(data.status)}
+						>
+							<Heading>{`Status: ${data.status}`}</Heading>
 						</Tile>
-						<Tile kind="ancestor">
-							<Tile kind="parent">
-								<Tile
-									kind="child"
-									renderAs={Notification}
-									color="grey"
-								>
-									<Heading subtitle>
-										{`Containers: `}
-										<span>{data.containers}</span>
-									</Heading>
-									{/* <Chart
-										type='line'
-										data={chartData}
-									/> */}
-								</Tile>
-							</Tile>
-						</Tile>
-						<Tile kind="ancestor">
-							<Tile kind="parent">
-								<Tile
-									kind="child"
-									renderAs={Notification}
-									color="grey"
-								>
-									<Heading subtitle>Side chain RPC nodes</Heading>
-									<Tabs>
-										<Tabs.Tab
-											onClick={() => setActiveNet('mainnet')}
-											active={activeNet === 'mainnet'}
-										>
-											N3 Mainnet
-										</Tabs.Tab>
-										<Tabs.Tab
-											onClick={() => setActiveNet('testnet')}
-											active={activeNet === 'testnet'}
-										>
-											N3 Testnet
-										</Tabs.Tab>
-									</Tabs>
-									<Table.Container>
-										<Table>
-											<thead>
-												<tr>
-													<th>
-														<abbr>
-															JSON RPC
-														</abbr>
-													</th>
-													<th>
-														<abbr>
-															Websocket RPC
-														</abbr>
-													</th>
-												</tr>
-											</thead>
-											<tbody>
-												{data.side_chain_rpc_nodes[activeNet].map((node) => (
-													<tr key={node[0]}>
-														<td>
-															{node[0]}
-														</td>
-														<td>
-															{node[1]}
-														</td>
-													</tr>
-												))}
-											</tbody>
-										</Table>
-										</Table.Container>
-								</Tile>
-							</Tile>
-						</Tile>
-					</Box>
-					<Box>
-						<Tile kind="ancestor">
-							<Tile kind="parent">
-								<Tile
-									kind="child"
-									renderAs={Notification}
-									color="grey"
-								>
-									<Heading subtitle>NeoFS deposit</Heading>
-									<Tabs>
-										<Tabs.Tab
-											onClick={() => setActiveNet('mainnet')}
-											active={activeNet === 'mainnet'}
-										>
-											N3 Mainnet
-										</Tabs.Tab>
-										<Tabs.Tab
-											onClick={() => setActiveNet('testnet')}
-											active={activeNet === 'testnet'}
-										>
-											N3 Testnet
-										</Tabs.Tab>
-									</Tabs>
-									<Heading subtitle size={6}>
-										<span>{`Address `}</span>
-										{data.contract[activeNet].address}
-									</Heading>
-									<Heading subtitle size={6}>
-										<span>{`Hash script `}</span>
-										{data.contract[activeNet].script_hash}
-									</Heading>
-								</Tile>
-							</Tile>
-						</Tile>
-					</Box>
-					<Box id="map">
-						<Tile kind="ancestor">
-							<Tile kind="parent">
-								<Tile
-									kind="child"
-									renderAs={Notification}
-									color="grey"
-								>
-									<Heading subtitle>Storage node map</Heading>
-									<MapContainer
-										center={[55, 15]}
-										zoom={5}
-										style={{
-											overflow: 'hidden',
-											height: 600,
-											borderRadius: 4,
-										}}
+						<Box style={{ marginTop: '1.5rem' }} id="main">
+							<Tile kind="ancestor">
+								<Tile kind="parent">
+									<Tile
+										kind="child"
+										renderAs={Notification}
+										color="grey"
 									>
-										<TileLayer
-											attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-											url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-										/>
-										<LayersControl position="bottomright">
-											{data.node_map.map((node) => (
-												<LayersControl.Overlay checked name={node.location} key={node.location}>
-													<Circle
-														center={[node.latitude, node.longitude]}
-														radius={10000}
-														pathOptions={node.nodes.reduce((previousValue, currentValue) => previousValue + Number(currentValue.value), 0) > 1 ? {
-															fillColor: 'green',
-														} : {
-															fillColor: 'red',
-														}}
-													>
-														<Popup>
-															<Heading
-																size={6}
-																align="center"
-																style={{ marginBottom: 10 }}
-															>
-																{node.location}
-															</Heading>
-															{node.nodes.map((node_item) => (
+										<Heading subtitle>
+											{`Network epoch: `}
+											<span>{data.network_epoch}</span>
+										</Heading>
+									</Tile>
+								</Tile>
+							</Tile>
+							<Tile kind="ancestor">
+								<Tile kind="parent">
+									<Tile
+										kind="child"
+										renderAs={Notification}
+										color="grey"
+									>
+										<Heading subtitle>
+											{`Containers: `}
+											<span>{data.containers}</span>
+										</Heading>
+										{/* <Chart
+											type='line'
+											data={chartData}
+										/> */}
+									</Tile>
+								</Tile>
+							</Tile>
+							<Tile kind="ancestor">
+								<Tile kind="parent">
+									<Tile
+										kind="child"
+										renderAs={Notification}
+										color="grey"
+									>
+										<Heading subtitle>Side chain RPC nodes</Heading>
+										<Tabs>
+											<Tabs.Tab
+												onClick={() => setActiveNet('mainnet')}
+												active={activeNet === 'mainnet'}
+											>
+												N3 Mainnet
+											</Tabs.Tab>
+											<Tabs.Tab
+												onClick={() => setActiveNet('testnet')}
+												active={activeNet === 'testnet'}
+											>
+												N3 Testnet
+											</Tabs.Tab>
+										</Tabs>
+										<Table.Container>
+											<Table>
+												<thead>
+													<tr>
+														<th>
+															<abbr>
+																JSON RPC
+															</abbr>
+														</th>
+														<th>
+															<abbr>
+																Websocket RPC
+															</abbr>
+														</th>
+													</tr>
+												</thead>
+												<tbody>
+													{data.side_chain_rpc_nodes[activeNet].map((node) => (
+														<tr key={node[0]}>
+															<td>
+																{node[0]}
+															</td>
+															<td>
+																{node[1]}
+															</td>
+														</tr>
+													))}
+												</tbody>
+											</Table>
+											</Table.Container>
+									</Tile>
+								</Tile>
+							</Tile>
+						</Box>
+						<Box>
+							<Tile kind="ancestor">
+								<Tile kind="parent">
+									<Tile
+										kind="child"
+										renderAs={Notification}
+										color="grey"
+									>
+										<Heading subtitle>NeoFS deposit</Heading>
+										<Tabs>
+											<Tabs.Tab
+												onClick={() => setActiveNet('mainnet')}
+												active={activeNet === 'mainnet'}
+											>
+												N3 Mainnet
+											</Tabs.Tab>
+											<Tabs.Tab
+												onClick={() => setActiveNet('testnet')}
+												active={activeNet === 'testnet'}
+											>
+												N3 Testnet
+											</Tabs.Tab>
+										</Tabs>
+										<Heading subtitle size={6}>
+											<span>{`Address `}</span>
+											{data.contract[activeNet].address}
+										</Heading>
+										<Heading subtitle size={6}>
+											<span>{`Hash script `}</span>
+											{data.contract[activeNet].script_hash}
+										</Heading>
+									</Tile>
+								</Tile>
+							</Tile>
+						</Box>
+						<Box id="map">
+							<Tile kind="ancestor">
+								<Tile kind="parent">
+									<Tile
+										kind="child"
+										renderAs={Notification}
+										color="grey"
+									>
+										<Heading subtitle>Storage node map</Heading>
+										<MapContainer
+											center={[55, 15]}
+											zoom={5}
+											style={{
+												overflow: 'hidden',
+												height: 600,
+												borderRadius: 4,
+											}}
+										>
+											<TileLayer
+												attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+												url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+											/>
+											<LayersControl position="bottomright">
+												{data.node_map.map((node) => (
+													<LayersControl.Overlay checked name={node.location} key={node.location}>
+														<Circle
+															center={[node.latitude, node.longitude]}
+															radius={10000}
+															pathOptions={node.nodes.reduce((previousValue, currentValue) => previousValue + Number(currentValue.value), 0) > 1 ? {
+																fillColor: 'green',
+															} : {
+																fillColor: 'red',
+															}}
+														>
+															<Popup>
 																<Heading
-																	key={node_item.net}
 																	size={6}
 																	align="center"
-																	weight="normal"
-																	style={{ marginBottom: 5 }}
+																	style={{ marginBottom: 10 }}
 																>
-																	{`${node_item.net === 'main' ? 'Mainnet' : 'Testnet'}: ${node_item.value} node${node_item.value > 1 ? 's' : ''}`}
+																	{node.location}
 																</Heading>
-															))}
-														</Popup>
-													</Circle>
-												</LayersControl.Overlay>
-											))}
-										</LayersControl>
-									</MapContainer>
+																{node.nodes.map((node_item) => (
+																	<Heading
+																		key={node_item.net}
+																		size={6}
+																		align="center"
+																		weight="normal"
+																		style={{ marginBottom: 5 }}
+																	>
+																		{`${node_item.net === 'main' ? 'Mainnet' : 'Testnet'}: ${node_item.value} node${node_item.value > 1 ? 's' : ''}`}
+																	</Heading>
+																))}
+															</Popup>
+														</Circle>
+													</LayersControl.Overlay>
+												))}
+											</LayersControl>
+										</MapContainer>
+									</Tile>
 								</Tile>
 							</Tile>
-						</Tile>
-					</Box>
-				</Section>
-			</Container>
-			<Footer
-				style={{ padding: '40px 20px' }}
-			>
-				<Heading
-					size={6}
-					weight="light"
-					subtitle
-					align="center"
-				>
-					Morphbits status monitoring page
-				</Heading>
-			</Footer>
+						</Box>
+					</Section>
+					</Container>
+					<Footer
+						style={{ padding: '40px 20px' }}
+					>
+						<Heading
+							size={6}
+							weight="light"
+							subtitle
+							align="center"
+						>
+							Morphbits status monitoring page
+						</Heading>
+					</Footer>
+				</>
+			)}
     </>
   );
 }
