701e0c883c7a2d64710112fa90fa47ddb27f167b
Mikhail Petrov <mike@nspcc.ru>
2022-06-01 13:27:39 +0300

    [#16] Add valid json for stdout
    Signed-off-by: Mikhail Petrov <mike@nspcc.ru>

--- get_webstat_metrics.py
+++ get_webstat_metrics.py
@@ -12,39 +12,39 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     output = {
-        'status': 'Unknown',
-        'network_epoch': 'unknown',
-        'containers': 'unknown',
-        'time': time.time(),
-        'node_map': [],
-        'contract': {
-            'mainnet': {
-                'address': 'NNxVrKjLsRkWsmGgmuNXLcMswtxTGaNQLk',
-                'script_hash': '2cafa46838e8b564468ebd868dcafdd99dce6221',
+        "status": "Unknown",
+        "network_epoch": "unknown",
+        "containers": "unknown",
+        "time": time.time(),
+        "node_map": [],
+        "contract": {
+            "mainnet": {
+                "address": "NNxVrKjLsRkWsmGgmuNXLcMswtxTGaNQLk",
+                "script_hash": "2cafa46838e8b564468ebd868dcafdd99dce6221",
             },
-            'testnet': {
-                'address': 'NadZ8YfvkddivcFFkztZgfwxZyKf1acpRF',
-                'script_hash': 'b65d8243ac63983206d17e5221af0653a7266fa1',
+            "testnet": {
+                "address": "NadZ8YfvkddivcFFkztZgfwxZyKf1acpRF",
+                "script_hash": "b65d8243ac63983206d17e5221af0653a7266fa1",
             }
         },
-        'side_chain_rpc_nodes': {
-            'mainnet': [
-                ['https://rpc1.morph.fs.neo.org:40341', 'wss://rpc1.morph.fs.neo.org:40341/ws'],
-                ['https://rpc2.morph.fs.neo.org:40341', 'wss://rpc2.morph.fs.neo.org:40341/ws'],
-                ['https://rpc3.morph.fs.neo.org:40341', 'wss://rpc3.morph.fs.neo.org:40341/ws'],
-                ['https://rpc4.morph.fs.neo.org:40341', 'wss://rpc4.morph.fs.neo.org:40341/ws'],
-                ['https://rpc5.morph.fs.neo.org:40341', 'wss://rpc5.morph.fs.neo.org:40341/ws'],
-                ['https://rpc6.morph.fs.neo.org:40341', 'wss://rpc6.morph.fs.neo.org:40341/ws'],
-                ['https://rpc7.morph.fs.neo.org:40341', 'wss://rpc7.morph.fs.neo.org:40341/ws'],
+        "side_chain_rpc_nodes": {
+            "mainnet": [
+                ["https://rpc1.morph.fs.neo.org:40341", "wss://rpc1.morph.fs.neo.org:40341/ws"],
+                ["https://rpc2.morph.fs.neo.org:40341", "wss://rpc2.morph.fs.neo.org:40341/ws"],
+                ["https://rpc3.morph.fs.neo.org:40341", "wss://rpc3.morph.fs.neo.org:40341/ws"],
+                ["https://rpc4.morph.fs.neo.org:40341", "wss://rpc4.morph.fs.neo.org:40341/ws"],
+                ["https://rpc5.morph.fs.neo.org:40341", "wss://rpc5.morph.fs.neo.org:40341/ws"],
+                ["https://rpc6.morph.fs.neo.org:40341", "wss://rpc6.morph.fs.neo.org:40341/ws"],
+                ["https://rpc7.morph.fs.neo.org:40341", "wss://rpc7.morph.fs.neo.org:40341/ws"],
             ],
-            'testnet': [
-                ['https://rpc01.morph.testnet.fs.neo.org:51331', 'wss://rpc01.morph.testnet.fs.neo.org:51331/ws'],
-                ['https://rpc02.morph.testnet.fs.neo.org:51331', 'wss://rpc02.morph.testnet.fs.neo.org:51331/ws'],
-                ['https://rpc03.morph.testnet.fs.neo.org:51331', 'wss://rpc03.morph.testnet.fs.neo.org:51331/ws'],
-                ['https://rpc04.morph.testnet.fs.neo.org:51331', 'wss://rpc04.morph.testnet.fs.neo.org:51331/ws'],
-                ['https://rpc05.morph.testnet.fs.neo.org:51331', 'wss://rpc05.morph.testnet.fs.neo.org:51331/ws'],
-                ['https://rpc06.morph.testnet.fs.neo.org:51331', 'wss://rpc06.morph.testnet.fs.neo.org:51331/ws'],
-                ['https://rpc07.morph.testnet.fs.neo.org:51331', 'wss://rpc07.morph.testnet.fs.neo.org:51331/ws'],
+            "testnet": [
+                ["https://rpc01.morph.testnet.fs.neo.org:51331", "wss://rpc01.morph.testnet.fs.neo.org:51331/ws"],
+                ["https://rpc02.morph.testnet.fs.neo.org:51331", "wss://rpc02.morph.testnet.fs.neo.org:51331/ws"],
+                ["https://rpc03.morph.testnet.fs.neo.org:51331", "wss://rpc03.morph.testnet.fs.neo.org:51331/ws"],
+                ["https://rpc04.morph.testnet.fs.neo.org:51331", "wss://rpc04.morph.testnet.fs.neo.org:51331/ws"],
+                ["https://rpc05.morph.testnet.fs.neo.org:51331", "wss://rpc05.morph.testnet.fs.neo.org:51331/ws"],
+                ["https://rpc06.morph.testnet.fs.neo.org:51331", "wss://rpc06.morph.testnet.fs.neo.org:51331/ws"],
+                ["https://rpc07.morph.testnet.fs.neo.org:51331", "wss://rpc07.morph.testnet.fs.neo.org:51331/ws"],
             ],
         },
     }
@@ -83,7 +83,7 @@ if __name__ == '__main__':
         output['node_map'] = map_node
         output['status'] = 'Healthy'
     except:
-        print('Connection error')
+        output['status'] = 'Unknown' # Connection error
     
     with (open(args.output, 'w') if args.output != '-' else sys.stdout) as handle:
-        handle.write(str(output))
+        handle.write(json.dumps(output, separators=(',', ':')))
